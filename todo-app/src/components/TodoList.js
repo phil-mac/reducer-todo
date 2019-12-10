f4c6932fdc2af6c808f1f53f7ec80c3123324f7c
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-12-10 14:36:44 -0800

    added a due date field where it shows items overdue status if applicable

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -7,14 +7,20 @@ export default () => {
     const [todoState, dispatch] = useReducer(reducer, initialState);
 
     const [newTodo, setNewTodo] = useState('')
+    const [newDueDate, setNewDueDate] = useState('');
     
     const handleChange = e => {
         setNewTodo(e.target.value)
     }
 
+    const handleTimeChange = e => {
+        console.log(e.target.value);
+        setNewDueDate(e.target.value);
+    }
+
     const handleSubmit = e => {
         e.preventDefault()
-        dispatch({type: "ADD_TODO", payload: newTodo})
+        dispatch({type: "ADD_TODO", payload: {item: newTodo, due: newDueDate}})
         setNewTodo('');
     }
 
@@ -31,11 +37,15 @@ export default () => {
             {todoState.todos.map(todo => (
                 <li key={todo.id} onClick={()=>toggleComplete(todo.id)}>
                     <span><strong>{todo.item}</strong></span>
-                    <span style={{fontSize: '80%', color: 'lightgrey'}}>  -  {moment(todo.id).fromNow()}</span>
+                    <span style={{fontSize: '80%', color: 'lightgrey'}}>  - created {moment(todo.id).fromNow()}</span>
+                    {moment(todo.due).fromNow().includes('ago') && 
+                        <span style={{fontSize: '80%', color: 'red'}}>  - due {moment(todo.due).fromNow()}</span>
+                    }
                     {todo.completed && <span style={{color: 'red'}}><strong> X</strong></span>}
                 </li>
             ))}
-            <li>
+            <div>
+                <hr/>
                 <form onSubmit={handleSubmit}>
                     <input 
                         type='text'
@@ -43,9 +53,19 @@ export default () => {
                         onChange={handleChange}
                         value={newTodo}
                     />
+                    <br/>
+                    <input 
+                        type='datetime-local'
+                        onChange={handleTimeChange}
+                    />
+                    {/* <input 
+                        type='time'
+                    /> */}
+                    <br/>
                     <button type='submit'>Add</button>
                 </form>
-            </li>
+            </div>
+            <hr/>
             <button onClick={clearCompleted}>Clear Completed</button>
         </ul>
     )
--- todo-app/src/reducers/todosReducer.js
+++ todo-app/src/reducers/todosReducer.js
@@ -3,12 +3,14 @@ export const initialState = {
         {
             item: 'Eat turkey',
             completed: true,
-            id: 1575012467650
+            id: 1575012467650,
+            due: '2019-12-01T17:23'
         },
         {
             item: 'Learn bout reducers',
             completed: false,
-            id: 1576015164650
+            id: 1576015164650,
+            due: '2019-12-18T17:23'
         },
     ]
 }
@@ -17,9 +19,10 @@ export const reducer = (state, action) => {
     switch(action.type){
         case('ADD_TODO'):
             const newTodo = {
-                item: action.payload,
+                item: action.payload.item,
                 completed: false,
-                id: Date.now()
+                id: Date.now(),
+                due: action.payload.due
             }
             return{
                 ...state,
